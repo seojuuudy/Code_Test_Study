5f0838fab8daf73b2c06594f36895c238c96b7da
seojuuudy <118946536+seojuuudy@users.noreply.github.com>
2023-04-24 15:46:02 +0900

    CodeUp 2회차 | [기초-산술연산] 6043-6045
    CodeUp 2회차 | [기초-산술연산] 6043-6045

---
+++ CodeUp/dayeong/6043_CodeUp.py
@@ -0,0 +1,5 @@
+﻿f1, f2 = input().split()
+f1 = float(f1)
+f2 = float(f2)
+f3 = f1 / f2
+print(format(f3,".3f"))
---
+++ CodeUp/dayeong/6045_CodeUp.py
@@ -0,0 +1,7 @@
+﻿a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+d = a + b + c
+print(a + b + c)
+print(format(d / 3, ".2f"))
