c164c40a5a9a33ad9583107a70f6fa6e0482d630
seojuuudy <118946536+seojuuudy@users.noreply.github.com>
2023-04-26 11:22:31 +0900

    3회차 | [기초-선택실행구조] 6065-6070
    3회차 | [기초-선택실행구조] 6065-6070

---
+++ CodeUp/dayeong/6068_CodeUp.py
@@ -0,0 +1,12 @@
+﻿n = int(input())
+if(n >= 90 and n <= 100) :
+    print('A')
+else :
+    if(n >= 70) :
+        print('B')
+    else :
+        if(n >= 40) :
+            print('C')
+        else :
+            if(n >= 0) :
+                print('D')
---
+++ CodeUp/dayeong/6070_CodeUp.py
@@ -0,0 +1,12 @@
+﻿a = input()
+if(a == '12' or a == '1' or a == '2') :
+    print('winter')
+else :
+    if(a == '3' or a == '4' or a == '5') :
+        print('spring')
+    else :
+        if(a == '6' or a == '7' or a == '8') :
+            print('summer')
+        else :
+            if(a == '10' or a == '9' or a == '11') :
+                print('fall')
