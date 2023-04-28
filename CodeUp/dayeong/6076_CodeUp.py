30f15d45c9cd38eb5edd1ae066f6d376dce82a33
seojuuudy <118946536+seojuuudy@users.noreply.github.com>
2023-04-28 20:47:31 +0900

    4회차 | [기초-반복실행구] 6071 - 6076
    4회차 | [기초-반복실행구] 6071 - 6076

---
+++ CodeUp/dayeong/6076_CodeUp.py
@@ -0,0 +1,7 @@
+﻿# for문으로 출력
+a = int(input()) # 입력값
+for i in range(a+1) :
+    print(i)
+# range(n) 은 0, 1, 2, ... , n-2, n-1 
+# range(n)에 들어있는(in) 각각의 수에 대해서(for) 순서대로 i에 저장
+# range(끝) range(시작, 끝) range(시작, 끝, 증감)
