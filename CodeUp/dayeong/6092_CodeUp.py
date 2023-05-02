73c09e32d2653a85fa38c5bf7d686b79c0cf0805
seojuuudy <118946536+seojuuudy@users.noreply.github.com>
2023-05-02 21:24:40 +0900

    5회차 | [기초-종합] 6092-6098
    5회차 | [기초-종합] 6092-6098

---
+++ CodeUp/dayeong/6092_CodeUp.py
@@ -0,0 +1,11 @@
+﻿n = int(input())
+a = input().split()
+d = []
+for i in range(n) : # 0부터 n-1까지
+    a[i] = int(a[i]) # 배열에 i 값추가 # a에 순서대로 저장
+for i in range(24) : # 0부터 23까지
+    d.append(0)
+for i in range(n) :
+    d[a[i]] = d[a[i]] + 1
+for i in range(1, 24) :
+    print(d[i], end=' ') 
