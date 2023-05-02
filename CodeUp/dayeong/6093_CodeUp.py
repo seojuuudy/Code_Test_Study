5c8be13c3286b5a507f8b422c89c73477b409980
seojuuudy <118946536+seojuuudy@users.noreply.github.com>
2023-05-02 21:40:47 +0900

    5회차 | [기초-리스트] 6092-6098
    5회차 | [기초-리스트] 6092-6098

---
+++ CodeUp/dayeong/6093_CodeUp.py
@@ -0,0 +1,8 @@
+﻿n = int(input())
+a = input().split()
+d = []
+for i in range(n) : # 0부터 n-1까지
+    a[i] = int(a[i]) # 배열에 i 값추가 # a에 순서대로 저장
+for i in range(n-1, -1, -1) : # 시작값은 입력값 -1 부터 -1 까지 -1 씩 감소한 값
+    # n-1, n-2, ..., 3, 2, 1, 0
+    print(a[i], end= ' ')
---
+++ CodeUp/dayeong/6094_CodeUp 오답.py
@@ -0,0 +1,8 @@
+﻿n = int(input())
+a = input().split()
+d = []
+for i in range(n) : # 0부터 n-1까지
+    a[i] = int(a[i]) # 배열에 i 값추가 # a에 순서대로 저장
+while d > a[i] :
+    d = a[i]
+print(d)   
