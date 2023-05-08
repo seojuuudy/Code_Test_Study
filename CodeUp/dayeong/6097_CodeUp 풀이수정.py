c46c8f11be0a86fed51e182f1e5655ca64a19a0c
seojuuudy <118946536+seojuuudy@users.noreply.github.com>
2023-05-09 08:23:55 +0900

    풀이 수정 | CodeUp 9097
    풀이 수정 | CodeUp 9097

---
+++ CodeUp/dayeong/6097_CodeUp 풀이수정.py
@@ -0,0 +1,24 @@
+﻿h, w = map(int, input().split())
+
+# h, w 크기 만큼의 격자판 생성
+arr = []
+for i in range(h+1) :
+  arr.append([])
+  for j in range(w+1) :
+    arr[i].append(0)
+
+n = int(input()) # 막대 개수
+for i in range(n) :
+  l,d,x,y = map(int, input().split()) #막대길이, 방향, 좌표 입력
+  # ㅣ,d 값이 막대의 형태를 정해주고 x,y 좌표값에 표시
+  if d == 0 : 
+    for j in range(l) :
+      arr[x][y+j] = 1
+  else :
+    for j in range(l) :
+      arr[x+j][y] = 1
+# 최종 값 출력      
+for i in range(1, h+1) :
+  for j in range(1, w+1) :
+    print(arr[i][j], end = ' ')
+  print()
